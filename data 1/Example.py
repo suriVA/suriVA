5ef1ff5ead32962d86c2078f874a8678e8bd40c1
suriVA <saisagar.surve@gmail.com>
2025-01-21 09:19:14 +0530

    Resume upload

--- data 1/Example.py
+++ data 1/Example.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sklearn as sk  
 
-df = pd.read_csv("E:/Project files/suriVA/TEST DATA.csv")
-y= df("logs")
-x= df.drop('Logs', axis=1)
+df = pd.read_csv('data 1/TEST DATA1.csv')
+y= df['logS']
+x= df.drop('logS', axis=1)
 
 #Splitting data into Training set and Testing set
 from sklearn.model_selection import train_test_split
@@ -26,8 +26,7 @@ lr_train_r2 = r2_score(Y_train, y_lr_train_pred)
 lr_test_mse = mean_squared_error(Y_test, y_lr_test_pred)
 lr_test_r2 = r2_score(Y_test, y_lr_test_pred)
 
-lr_results= pd.DataFrame(('Liner Regression', lr_train_mse, lr_train_r2, lr_test_r2)).transpose()
-lr_results.columns =["Method", "Trainig MSE", "Training R2", "Test MSE", "Test R2"]
+lr_results = pd.DataFrame([('Liner Regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2)], columns=['Method', 'Trainig MSE', 'Training R2', 'Test MSE', 'Test R2'])
 
 #Random Forest Liner Regression model
 from sklearn.ensemble import RandomForestRegressor
@@ -45,8 +44,7 @@ rf_train_r2 = r2_score(Y_train, y_rf_train_pred)
 rf_test_mse = mean_squared_error(Y_test, y_rf_test_pred)
 rf_test_r2 = r2_score(Y_test, y_rf_test_pred)
 
-rf_results= pd.DataFrame(('Liner Regression', rf_train_mse, rf_train_r2, rf_test_r2)).transpose()
-rf_results.columns =["Method", "Trainig MSE", "Training R2", "Test MSE", "Test R2"]
+rf_results= pd.DataFrame([('Liner Regression', rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2)], columns=['Method', 'Trainig MSE', 'Training R2', 'Test MSE', 'Test R2'])
 
 #Combine Results
 df_models = pd.concat([lr_results, rf_results], axis=1)
@@ -56,8 +54,8 @@ df_models.reset_index(drop=True)
 import matplotlib.pyplot as plt
 import numpy as np
 plt.scatter(x=Y_train, y=y_rf_train_pred, alpha=0.3)
-z= np.polyfit(Y_train, y_lr_train_pred, 1)
-p= np.polyld(z)
+z = np.polyfit(Y_train, y_lr_train_pred, 1)
+p = np.poly1d(z)
 
 plt.plot(Y_train, p(Y_train), '#F8766D')
 plt.ylabel('Predict LogS')
